d769026967d0243f2a8d1009ea08d578841d3bc6
Dregron <Andymurray2007@gmail.com>
2016-03-04 12:52:48 +0000

    cleaning up utils

--- src/com/dregronprogram/utils/MathUtils.java
+++ src/com/dregronprogram/utils/MathUtils.java
@@ -5,10 +5,4 @@ public class MathUtils {
 	public static boolean isEqual(int a, int b, int tolerances) {
 		return Math.abs(a - b) <= tolerances;
 	}
-
-	public static float lerp(float targetPos, float currentPos, float progress) {
-		if (targetPos - currentPos > 0) return currentPos + progress;
-		else if (targetPos - currentPos < 0) return currentPos - progress;
-		return currentPos;
-	}
 }
